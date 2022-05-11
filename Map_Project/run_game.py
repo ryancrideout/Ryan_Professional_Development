95063a9e08557d55b4db13d8e83d18477f90d3dd
ryancrideout <ryancrideout@gmail.com>
2022-05-10 20:38:57 -0600

    Okay now we have something that runs. Yay us!

--- Map_Project/run_game.py
+++ Map_Project/run_game.py
@@ -13,3 +13,16 @@ from abc import ABC, abstractmethod
 class Game():
     def shoryuken():
         print("Shoryuken")
+
+class Map(ABC):
+    @abstractmethod
+    def shin_shoryuken():
+        print("Shin Shoryuken")
+
+# The main function where everything starts.
+def main():
+    print("I am the duke now.")
+
+# Actually run this blasted thing.
+if __name__ == "__main__":
+    main()
