f9ad1ee227679e8b158a4d6c9d32ce8e155356e2
ryancrideout <ryancrideout@gmail.com>
2022-05-10 20:34:22 -0600

    Whoops forgot to make a new branch.

--- Map_Project/run_game.py
+++ Map_Project/run_game.py
@@ -1,1 +1,15 @@
-# Let the games begin!
+"""
+Things to do:
+- Create a Game Class
+- Create an abstract Map Class
+  - Make a "Plain" Subsclass
+- Create an abstract Character Class
+  - Make some character classes
+- Cleanup, put these into folders and stuff.
+"""
+
+from abc import ABC, abstractmethod
+
+class Game():
+    def shoryuken():
+        print("Shoryuken")
