5ee41bc5b7c6da505833534024808290185e44dd
Ryan Rideout <ryancrideout@gmail.com>
2022-05-13 12:30:35 -0600

    Added logic to let the game move characters and stuff.

--- Map_Project/classes/game.py
+++ Map_Project/classes/game.py
@@ -1,6 +1,13 @@
 from classes.plain import Plain
+from classes.plebian import Plebian
 
 class Game():
+    def __init__(self):
+        self.map = None
+
+    def set_map(self, map):
+        self.map = map
+
     def run(self):
         """
         Okay we're going to have to figure out how to initialize this whole thing...
@@ -10,9 +17,70 @@ class Game():
         plain = Plain()
         # Make this a user input
         plain.initialize(10, 10)
-        # Yay this works
+
+        self.set_map(plain)
+
+        # Render the map
+        # plain.render()
+
+        leonidas = Plebian()
+        # Set coordinates, but make this a user input.
+        leonidas.set_position(3, 3)
+        self.add_entity_to_map(leonidas)
         plain.render()
+
+        print()
+
+        self.move_character(leonidas, "South")
+        plain.render()
+        # Ask for user input.
 
         # TODO: Flesh this out some more, actually make this a "game",
         #       ask user for prompts and then add characters and then
         #       make them move.
+
+    def move_character(self, character, direction):
+        desired_position = character.movement_action(direction)
+        new_x_cord = desired_position[0]
+        new_y_cord = desired_position[1]
+
+        # Check if desired spot is occupied.
+        # TODO: Out of bounds error checking.
+        if self.check_if_occupied(new_x_cord, new_y_cord):
+            print("Cannot move to location! Already occupied.")
+        else:
+            # Move the character. First, remove character from map.
+            self.remove_entity_from_map(character)
+            # Set the character's new position
+            character.set_position(new_x_cord, new_y_cord)
+            # Add the character back to the map.
+            self.add_entity_to_map(character)
+
+    def add_entity_to_map(self, entity):
+        """
+        This takes an entity (E.G., Character) and adds it to the map.
+        """
+        x_cord = entity.x
+        y_cord = entity.y
+
+        # Need to do - add error checking.
+        if not self.map.grid[x_cord][y_cord].occupant:
+            self.map.grid[x_cord][y_cord].set_occupant(entity)
+
+    def remove_entity_from_map(self, entity):
+        """
+        We're making an assumption with this method that the entity already
+        exists on the map.
+        """
+        x_cord = entity.x
+        y_cord = entity.y
+
+        # Again, need to add some error checking or something.
+        if self.map.grid[x_cord][y_cord].occupant:
+            self.map.grid[x_cord][y_cord].set_occupant(None)
+
+    def check_if_occupied(self, x_cord, y_cord):
+        if self.map.grid[x_cord][y_cord].occupant:
+            return True
+        else:
+            return False
---
+++ Map_Project/classes/plebian.py
@@ -0,0 +1,27 @@
+from classes.abstract.character import Character
+
+class Plebian(Character):
+    def __init__(self):
+        self.x = None
+        self.y = None
+        self.position = None
+        self.icon = "P"
+
+    def set_position(self, x, y):
+        self.position = (x, y)
+        self.x = x
+        self.y = y
+
+    def movement_action(self, direction):
+        """
+        Set of instructions on how Plebians move. Plebians
+        don't have sophisticated movement, as demonstrated.
+        """
+        if direction.lower() in self.UP:
+            return (self.x, (self.y + 1))
+        elif direction.lower() in self.DOWN:
+            return (self.x, (self.y - 1))
+        elif direction.lower() in self.LEFT:
+            return ((self.x - 1), self.y)
+        elif direction.lower() in self.RIGHT:
+            return ((self.x + 1), self.y)
