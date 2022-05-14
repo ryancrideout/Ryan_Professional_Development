1272da437c0c12d6a0556ff566675b68367b9e88
ryancrideout <ryancrideout@gmail.com>
2022-05-14 12:18:51 -0600

    Removed some comments.

--- Map_Project/run_game.py
+++ Map_Project/run_game.py
@@ -17,16 +17,6 @@ from classes.game import Game
 def main():
     map_game = Game()
     map_game.run()
-    # Should characters have a maptile object?
-    # What about the map they're on?
-    # Maybe I could just give them a "position" attribute...
-    # But even so, moving them is going to require the map (maptiles plural) to be updated.
-    # MAYBE I PUT "MOVE CHARACTERS" on the map command?
-    # I think maybe we let the game handle all of that, and then it updates the map accordingly.
-    # Ugh that's going to be messy.
-
-    # Okay new plan - let the game handle movement, but the player classes will have instructions
-    # on how to move.
 
 # Actually run this blasted thing.
 if __name__ == "__main__":
