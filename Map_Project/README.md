eefe9ec4461d9e826d8d6c7e5e7bc0be3679b019
ryancrideout <ryancrideout@gmail.com>
2022-06-08 20:58:14 -0600

    Added some groundwork, but the real work is going to continue in phase_2

---
+++ Map_Project/README.md
@@ -0,0 +1,5 @@
+## How to run Unittests
+
+While being in the `Map_Project` directory (I.E., this directory), simply run `python -m unittest discover`.
+
+I might have plans in the future to make this something you can do in Pipenv Shell or something, I don't know.
---
+++ Map_Project/tests/test_game.py
@@ -0,0 +1,38 @@
+import unittest
+from classes.game import Game
+
+class TestGame(unittest.TestCase):
+    '''
+    NOTE: We're going to have to figure out how to do user inputs for 
+          unit tests.
+    '''
+    def test_run(self):
+        # This might be hard to test due to user inputs
+        pass
+
+    def test_set_map(self):
+        pass
+
+    def test_add_entity_to_map(self):
+        pass
+
+    def test_remove_entity_to_map(self):
+        pass
+
+    def test_check_if_occupied(self):
+        pass
+
+    def test_initialize_map(self):
+        pass
+
+    def test_create_character(self):
+        pass
+
+    def test_set_character_position(self):
+        pass
+
+    def test_move_character(self):
+        pass
+
+    def test_display_game_commands(self):
+        pass
