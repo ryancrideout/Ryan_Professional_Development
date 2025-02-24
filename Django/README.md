2c882c746fa879557c54c6b5d41f65f50c483058
Ryan Rideout <ryan.rideout@trialcard.com>
2025-02-24 09:45:10 -0700

    Did tutorials 6 and 7

--- Django/README.md
+++ Django/README.md
@@ -4,6 +4,11 @@ This is just a Django tutorial so that I can become familiar with Django.
 
 Worth noting that you will need Django to run this project, and that can be accomplished with `python -m pip install Django`.
 
+Because I'm forgetful, this is the username and password for Django admin:
+`Username: ryan.rideout`
+`Password: machinegunkiss`
+`Email: ryan.rideout@example.com`
+
 Tutorials:
 [Official Django Tutorial](https://docs.djangoproject.com/en/5.1/intro/tutorial01/)
 [w3school Tutorial](https://www.w3schools.com/django/)
--- Django/polls/admin.py
+++ Django/polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["publish_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["publish_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ["question_text", "publish_date", "was_published_recently"]
+    list_filter = ["publish_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
